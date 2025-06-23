99c3150135331262f93f0c537c18b4df319822b9
RigelTLT <kozyrin-di@yandex.ru>
2025-06-23 15:42:17 +0400

    fix: delete files

--- Новая папка/backend/routes/containers.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { verifyJWT } = require("../middleware/auth");
-const { listContainers } = require("../controllers/containerController");
-
-router.get("/", verifyJWT, listContainers);
-
-module.exports = router;
